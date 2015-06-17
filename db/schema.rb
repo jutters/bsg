a7fc940173b11c0d5a2577a2ba110703deca4681
jutters <juetterschenke@gmx.net>
2015-06-17 15:36:21 +0200

    set all flags of a shop to false by default

--- db/schema.rb
+++ db/schema.rb
@@ -20,15 +20,15 @@ ActiveRecord::Schema.define(version: 20150408164348) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "price_segment_low",               default: false
-    t.boolean  "price_segment_middle",            default: true
+    t.boolean  "price_segment_middle",            default: false
     t.boolean  "price_segment_high",              default: false
-    t.boolean  "locale_de_shop",                  default: true
+    t.boolean  "locale_de_shop",                  default: false
     t.boolean  "locale_foreign_shop_de_website",  default: false
     t.boolean  "locale_foreign_shop_de_delivery", default: false
     t.boolean  "brand_type_single",               default: false
-    t.boolean  "brand_type_multi",                default: true
-    t.boolean  "shop_type_women_fashion",         default: true
-    t.boolean  "shop_type_men_fashion",           default: true
+    t.boolean  "brand_type_multi",                default: false
+    t.boolean  "shop_type_women_fashion",         default: false
+    t.boolean  "shop_type_men_fashion",           default: false
     t.boolean  "shop_type_interior_design",       default: false
     t.boolean  "insider_tip",                     default: false
     t.string   "image"

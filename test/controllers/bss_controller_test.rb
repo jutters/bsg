57e235dfd9e6c01f396e78469533630e8dda8fa1
jutters <juetterschenke@gmx.net>
2015-07-09 00:02:52 +0200

    fix bss controller tests to reflect the existence of the men/women/interior pages (instead of one index page)

--- test/controllers/bss_controller_test.rb
+++ test/controllers/bss_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class BssControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
+  test "should get men" do
+    get :men
     assert_response :success
   end
 
+  test "should get women" do
+    get :women
+    assert_response :success
+  end
+
+  test "should get interior" do
+    get :interior
+    assert_response :success
+  end
+  
 end
